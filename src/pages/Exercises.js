6d55ca62d697b5241534d5bf83b42f4ea192f7e5
Jordy Tirado <jordytiradotorres20@gmail.com>
2020-02-18 12:25:12 -0500

    first commit

---
+++ src/pages/Exercises.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Welcome from "../components/Welcome";
+import ExerciseList from "../components/ExerciseList";
+import AddButtom from "../components/AddButton";
+
+const Exercises = ({ data }) => (
+  <Fragment>
+    <Welcome username="Jordy Tirado" />
+    <ExerciseList exercises={data} />
+    <AddButtom />
+  </Fragment>
+);
+
+export default Exercises;
